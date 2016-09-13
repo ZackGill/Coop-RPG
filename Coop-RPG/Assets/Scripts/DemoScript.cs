b12a6316d555e027e2f06b449b87bb0d1c196108
Zack Gill <zack@yeticgi.com>
2016-09-12 20:02:05 -0400

    Intial Prototype
    This is a prototype for the project. It includes some sprites, prefabs
    for walls and floors, an example script, some example Hello World stuff.

---
+++ Coop-RPG/Assets/Scripts/DemoScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DemoScript : MonoBehaviour {
+
+    public Text hello;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hello.text = "Hello World";
+
+        print("This is console printing");
+	}
+}
