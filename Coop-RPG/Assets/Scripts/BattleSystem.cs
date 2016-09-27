478a18aa2870b3f48c50cb4f1228f6151d845389
heiner15 <heiner@mail.gvsu.edu>
2016-09-27 08:31:47 -0400

    Added two battle related skeletons

---
+++ Coop-RPG/Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class BattleSystem : MonoBehaviour {
+
+	List<CombatMove> moveQueue = List<BattleMove>();
+	List<Player> players = List<Player>();
+	List<Enemy> enemies = List<Enemy>();
+
+	// Use this for initialization
+	void Start () {
+		while (enemies != null || players != null) {
+			// players choose moves
+			foreach (Player player in players) {
+				moveQueue.Add (player.ChooseMove ());
+			}
+			// enemies choose moves
+			foreach (Enemy enemy in enemies) {
+				moveQueue.Add (enemy.ChooseMove ());
+			}
+			// sort and execute
+			//moveQueue.Sort (); //C# has built in sort function for list
+			ExecuteMoves();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//render every tick
+	}
+
+	// Goes down list and perfroms each move
+	void ExecuteMoves(List<CombatMove> moves) {
+		foreach (CombatMove move in moves) {
+			// execute
+		}
+	}
+}
---
+++ Coop-RPG/Assets/Scripts/CombatMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatMove : MonoBehaviour {
+
+	Entity caster;
+	List<Entity> targets = List<Entity>();
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
