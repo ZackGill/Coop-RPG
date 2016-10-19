4862887e0d1dca044ed60d35f83e7300f875c6cd
Lex Leesch <Leescha@mail.gvsu.edu>
2016-10-19 16:49:43 -0400

    I did nothing at all!

---
+++ Coop-RPG/Assets/Scripts/BattleScreenScripts/EnemyQuantity.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyQuantity : MonoBehaviour {
+
+    int numberOfEnemies;
+
+    // Use this for initialization
+    void Start () {
+        numberOfEnemies = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public int getNumberOfEnemies()
+    {
+        return numberOfEnemies;
+    }
+}
