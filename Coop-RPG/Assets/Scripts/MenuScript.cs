4f8a32031346cb421afd532f8435873dd9358f94
Zack Gill <zack@yeticgi.com>
2016-10-26 16:32:27 -0400

    Revert "Revert "Changes""
    This reverts commit 162ce0d612382198a1a422cf2c5fcb62e47f82b4.

--- Coop-RPG/Assets/Scripts/MenuScript.cs
+++ Coop-RPG/Assets/Scripts/MenuScript.cs
@@ -3,8 +3,8 @@ using System.Collections;
 using SimpleFirebaseUnity;
 using SimpleFirebaseUnity.MiniJSON;
 using UnityEngine.UI;
-namespace AssemblyCSHarp
-{
+using System.Collections.Generic;
+using AssemblyCSharp;
     public class MenuScript : MonoBehaviour
     {
 
@@ -15,12 +15,15 @@ namespace AssemblyCSHarp
         static int debug_idx = 0;
         string userName, password;
 
+    Firebase fire;
+
         // Use this for initialization
         void Start()
         {
-            // TODO: Firebase stuff here, mainly to set it up so once login or create is clicked, can do firebase stuff.
-            // Also call character list stuff. Do as much as possible at a time, elminate waiting latter.
-        }
+        fire = Firebase.CreateNew("coop-rpg.firebaseio.com/Accounts", "nofP6v645gh35aA1jlQGOc4ueceuDZqEIXu7qMs1");
+        // TODO: Firebase stuff here, mainly to set it up so once login or create is clicked, can do firebase stuff.
+        // Also call character list stuff. Do as much as possible at a time, elminate waiting latter.
+    }
 
         // Update is called once per frame
         void Update()
@@ -52,4 +55,3 @@ namespace AssemblyCSHarp
         }
 
     }
-}
