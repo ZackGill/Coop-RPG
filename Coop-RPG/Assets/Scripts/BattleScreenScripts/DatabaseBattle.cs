32dc3e16a259133ba08b0a02a88fa7dcb9619724
Zack Gill <zack@yeticgi.com>
2016-11-10 23:21:00 -0500

    fixed bugs

--- Coop-RPG/Assets/Scripts/BattleScreenScripts/DatabaseBattle.cs
+++ Coop-RPG/Assets/Scripts/BattleScreenScripts/DatabaseBattle.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class DatabaseBattle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private Characters character;
+    DatabaseManager db = new DatabaseManager();
+
+    // Use this for initialization
+    void Start () {
+
+        character = GetComponent<Characters>();
+        StartCoroutine(wait());
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    IEnumerator wait()
+    {
+        character = null;
+        StartCoroutine(db.runChar("Lex"));
+        yield return new WaitForSeconds(35f);
+        character = db.getChar();
+    }
+
+    public Characters getCharacter()
+    {
+        return character;
+    }
 }
